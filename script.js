be54dd29553c419607c6f81916dee3aa08d9a702
cleyson <cleyson.ribeiro.777@gmail.com>
2024-11-24 22:11:47 -0300

    teste

--- script.js
+++ script.js
@@ -1,17 +1,41 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a")
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && offset + height) {
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a [href*=' + id + ' ]').classList.add('active')
+            })
+        }
+    })
+}
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+
 document.addEventListener("DOMContentLoaded", () => {
-    const filterButtons = document.querySelectorAll(".btn-filter");  // Classe correta
+    const filterButtons = document.querySelectorAll(".btn-filter");
     const skillItems = document.querySelectorAll(".skill-item");
 
     filterButtons.forEach(button => {
         button.addEventListener("click", () => {
-            // Remove a classe 'active' de todos os botões
             filterButtons.forEach(btn => btn.classList.remove("active"));
-            // Adiciona a classe 'active' ao botão clicado
             button.classList.add("active");
 
             const filter = button.getAttribute("data-filter");
 
-            // Mostrar ou ocultar os itens com base no filtro
             skillItems.forEach(item => {
                 if (filter === "all" || item.classList.contains(filter)) {
                     item.style.display = "block";
@@ -24,11 +48,9 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-    // Adiciona a classe 'active' ao botão 'Back-end' quando a página for carregada
     const backEndButton = document.querySelector('[data-filter="back-end"]');
     backEndButton.classList.add('active');
 
-    // Filtra as habilidades para exibir apenas as de 'back-end' por padrão
     filterSkills('back-end');
 });
 
@@ -43,13 +65,11 @@ function filterSkills(filter) {
     });
 }
 
-// Adiciona a funcionalidade de filtro nos botões
 const filterButtons = document.querySelectorAll('.btn-filter');
 filterButtons.forEach(button => {
     button.addEventListener('click', function () {
         filterSkills(button.getAttribute('data-filter'));
-        
-        // Remove a classe 'active' de todos os botões e adiciona no botão clicado
+
         filterButtons.forEach(b => b.classList.remove('active'));
         button.classList.add('active');
     });
